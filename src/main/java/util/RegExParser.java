81b987a8e255e9bbf32b62674fcdbdab473e1687
Валера Хлуд <valkhlud@gmail.com>
2022-10-21 16:34:00 +0300

    some changes

--- src/main/java/util/RegExParser.java
+++ src/main/java/util/RegExParser.java
@@ -4,9 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegExParser {
-    private StringBuilder parsedText = new StringBuilder();
 
-    public String getTegTextValue(String text) {
+
+    public static String getTegTextValue(String text) {
+        StringBuilder parsedText = new StringBuilder();
         Pattern pattern = Pattern.compile("[0-9]");
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
