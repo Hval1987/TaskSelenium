ad77568e58983844461959f990da4c31b49a076f
Hval1987 <valkhlud@gmail.com>
2022-10-23 22:00:07 +0300

    update theory

--- src/main/java/page/Theory.java
+++ src/main/java/page/Theory.java
@@ -1,14 +1,95 @@
-пакет com.cyborg.core.generic.dataUtils;
-импортировать java.io.FileInputStream;
-импортировать java.io.FileNotFoundException;
-импортировать java.io.IOException;
-импортировать java.io.InputStream;
-импортировать java.util.LinkedHashMap;
-импортировать java.util.Properties;
-импортировать java.util.Set;импортировать org.apache.log4j.PropertyConfigurator; // Этоpu класс SingleTon blic
-class PropertiesDataUtils {
+package com.cyborg.core.generic.dataUtils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.LinkedHashMap;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.log4j.PropertyConfigurator;
+
+// This is the SingleTon Class
+public class PropertiesDataUtils {
     private Properties properties = null;
-    публичный статический LinkedHashMap configDataStore = новый LinkedHashMap (); Входной поток равен нулю; // Это статическая и приватная ссылка на класс, которую вы можете использовать в любом месте вашего фреймворка private static PropertiesDataUtils propertiesDataUtils = null; логическое значение centralizeLog = false; // Это закрытый конструктор для создания объекта, но вы не можете получить к нему доступ извне класса, чтобы поддерживать дизайн шаблона SingleTon, т.е. создавать только один объект.
-private PropertiesDataUtils(String filePath){generateDataStore(filePath);
-CentralizeLog=Boolean.parseBoolean(PropertiesDataUtils.configDataStore.get("centralizedLog"));
-if(centralizeLog)PropertyConfigurator.configure(System.getProperty("user.dir")+"//src//test//resources//config//log4j_central.properties");else PropertyConfigurator.configure(System.getProperty("user.dir")+"//src//test//resources//config//log4j_local.properties");}private PropertiesDataUtils(){} // Этот метод в основном создает экземпляр класса SingleTon public static PropertiesDataUtils getInstance(String filePath) { if (propertiesDataUtils == null) propertiesDataUtils = new PropertiesDataUtils(filePath); вернуть свойстваDataUtils; } // этот метод в основном создает хранилище данных, в котором вы хотите хранить все данные конфигурации, как обсуждалось ранее private void generateDataStore(String filePath) { try { this.properties = new Properties(); = новый FileInputStream (filePath); свойства.load(есть); переопределить из среды (); Установлен ключи = загрузитьВсеКлючи(); for (Объект k: ключи) { String key = (String) k; configDataStore.put (ключ, getPropertyValue (ключ)); } } catch (FileNotFoundException fileNotFoundException) { String exceptionData = String.valueOf(fileNotFoundException.getCause().getMessage()); } catch (IOException ioException) { String exceptionData = String.valueOf(ioException.getCause().getMessage()); } наконец { if (null!= is) { try { is.close(); } catch (Exception e) { String exceptionData = String.valueOf(e.getCause().getMessage()); } } } } // Этот метод используется для загрузки всех ключей из файла свойств.
+    public static LinkedHashMap<String, String> configDataStore = new LinkedHashMap<String, String>();
+    InputStream is = null;
+    // This is the static and private reference of the class which you could use anywhere in you framework
+    private static PropertiesDataUtils propertiesDataUtils = null;
+    boolean centralizeLog = false;
+
+    // This is the Private constructor to create the object
+    // but you can not access this from outside the class
+    // to maintain the design of the SingleTon pattern ie only one object creation .
+    private PropertiesDataUtils(String filePath) {
+        generateDataStore(filePath);
+        centralizeLog = Boolean.parseBoolean(PropertiesDataUtils.configDataStore.get("centralizedLog"));
+        if (centralizeLog)
+            PropertyConfigurator.configure(System.getProperty("user.dir") + "//src//test//resources//config//log4j_central.properties");
+        else
+            PropertyConfigurator.configure(System.getProperty("user.dir") + "//src//test//resources//config//log4j_local.properties");
+    }
+
+    private PropertiesDataUtils() {
+    }
+
+
+    // This method basically create the instance of the SingleTon class
+    public static PropertiesDataUtils getInstance(String filePath) {
+        if (propertiesDataUtils == null)
+            propertiesDataUtils = new PropertiesDataUtils(filePath);
+        return propertiesDataUtils;
+    }
+
+    // this method basically creates the datastore
+    // where you want to store all the config data as discussed previously
+    private void generateDataStore(String filePath) {
+        try {
+            this.properties = new Properties();
+            is = new FileInputStream(filePath);
+            properties.load(is);
+            overrideFromEnvironment();
+            Set<Object> keys = loadAllKeys();
+            for (Object k : keys) {
+                String key = (String) k;
+                configDataStore.put(key, getPropertyValue(key));
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            String exceptionData = String.valueOf(fileNotFoundException.getCause().getMessage());
+        } catch (IOException ioException) {
+            String exceptionData = String.valueOf(ioException.getCause().getMessage());
+        } finally {
+            if (null != is) {
+                try {
+                    is.close();
+                } catch (Exception e) {
+                    String exceptionData = String.valueOf(e.getCause().getMessage());
+                }
+            }
+        }
+    }
+
+    // This method is used to load all the keys from the properties file.
+    private Set<Object> loadAllKeys() {
+        Set<Object> keys = this.properties.keySet();
+        return keys;
+    }
+
+    private String getPropertyValue(String key) {
+        return this.properties.getProperty(key);
+    }
+
+    private void setPropertyValue(String key, String value) {
+        this.properties.setProperty(key, value);
+    }
+
+    private void overrideFromEnvironment() {
+        if (this.properties.getProperty("run_on_jenkins").equals("true")) {
+            System.out.println("SET run_on_jenkins FLAG TO FALSE IN YOUR PROPERTIES FILE TO RUN LOCALLY....");
+            String build_number = System.getenv("BUILD_NUMBER");
+            this.properties.setProperty("build_number", build_number);
+        }
+
+    }
+}
